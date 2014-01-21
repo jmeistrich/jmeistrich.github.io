23d00aee629e11b0c9fd3f58190eb935147f8d63
Jay Meistrich <jmeistrich@gmail.com>
2014-01-21 16:37:05 +1300

    Fix up dates in itinerary

---
+++ itinerary.js
@@ -0,0 +1,9 @@
+var itinerary = [
+    '1/1',  'Maui, HI',
+    '1/4',  'Honolulu, HI',
+    '1/8',  'Seattle, WA',
+    '1/14', 'Auckland, NZ',
+    '1/20', 'Tauranga, NZ',
+    '1/22', 'Waitomo, NZ',
+    '1/23', 'Rotorua, NZ'
+];
