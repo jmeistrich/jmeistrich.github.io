2d3492ef284a56e936f8bfa928e3236bea460ef7
Jay Meistrich <jmeistrich@gmail.com>
2014-04-21 15:46:18 +0900

    Update itineraries

--- itinerary.js
+++ itinerary.js
@@ -16,5 +16,14 @@ var itinerary = [
     '2/7',  'Brisbane, AU',
     '2/8',  'Cruise in AU',
     '2/15', 'Brisbane, AU',
-    '2/17', 'Gold Coast, AU'
+    '2/17', 'Gold Coast, AU',
+    '2/23', 'Byron Bay, AU',
+    '3/1', 'Sydney, AU',
+    '3/12', 'Melbourne, AU',
+    '4/1', 'Noosa, AU',
+    '4/9', 'Cairns, AU',
+    '4/16', 'Hiroshima, JP',
+    '4/20', 'Osaka, JP',
+    '4/24', 'Kyoto, JP',
+    '4/29', 'Tokyo, JP'
 ];
