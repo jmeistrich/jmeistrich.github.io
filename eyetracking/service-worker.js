7126af1fc9898ab4270780fc31d9c78f3c65c669
Jay Meistrich <jmeistrich@gmail.com>
2019-04-30 17:23:28 -0700

    Fix relative paths

--- eyetracking/service-worker.js
+++ eyetracking/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6f81c50073967dbbd5fa79779be2457.js"
+  "/eyetracking/precache-manifest.6ef0eea873140f4e31c1ea40c5b6ce5e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eyetracking/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
